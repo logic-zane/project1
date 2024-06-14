972b18f19d92f4adc58410fc87f9e8a7f0b1c759
logic-zane <102068648+logic-zane@users.noreply.github.com>
2024-06-14 06:12:02 -0400

    Update Event.cpp

--- Event.cpp
+++ Event.cpp
@@ -1,80 +1,86 @@
 #include "Event.hpp"
+#include <algorithm>
 
+// Default constructor
+Event::Event() : name_("Not Set Yet"), timestamp_(-1) {}
 
+// Parameterized constructor
+Event::Event(const std::string& name, const long& timestamp) : name_(name), timestamp_(timestamp) {}
 
-Event::Event(){
-    this -> name_ = "Not Set Yet";
-    this -> timestamp_ = -1;
-}
-
-Event::Event(const std::string& name, const long& timestamp){
-    this -> name_ = name;
-    this -> timestamp_ = timestamp;
-}
-
-
-std::string Event::getName() const{
+// Get the name of the event
+std::string Event::getName() const {
     return name_;
 }
 
-long Event::getTimestamp() const{
+// Get the timestamp of the event
+long Event::getTimestamp() const {
     return timestamp_;
-
 }
-std::vector<std::string> Event::getGuests() const{
 
+// Get the list of guests for the event
+std::vector<std::string> Event::getGuests() const {
     return guests_;
 }
-void Event::setName(const std::string& name){
+
+// Set the event name
+void Event::setName(const std::string& name) {
     name_ = name;
 }
-bool Event::setTimestamp(const long& timestamp){
-    if(timestamp < 0){
+
+// Set the event timestamp
+bool Event::setTimestamp(const long& timestamp) {
+    if (timestamp < 0) {
         return false;
     }
     timestamp_ = timestamp;
     return true;
-    }
-bool Event::setGuests(const std::vector<std::string>& guests){
-    for(size_t i = 0; i< guests.size(); i++){
-        if(guests[i]==" "){
-            return false;
-        }
-        guests_ = guests;
-        return true;
-    }
-}
-std::ostream& operator<<(std::ostream& os, const Event& event){
-    os<< "Name: "<< event.name_ << "\n";
-    os<< "Time: "<< event.timeToString() << "\n";
-    os<< "Guest:"<< "\n";
-    for(size_t index = 0; index < event.guests_.size(); index++){
-        os<< index <<": " << event.guests_[index] << "\n";
-    }
-    os<< "-------------------------------------------"<<"\n";
-    return os;
-}
-std::istream& operator>>(std::istream& is, Event&event){
-    std:: string guest;
-    while(std::getline(is, guest)){
-        if(guest!=""){
-            event.guests_.push_back(guest);
-        }
-        return is;
-    }
 }
 
-bool Event::addGuest(const std::string& name){
-    if(name== ""){
+// Set the guests list
+bool Event::setGuests(const std::vector<std::string>& guests) {
+    for (const std::string& guest : guests) {
+        if (guest.empty()) {
+            return false;
+        }
+    }
+    guests_ = guests;
+    return true;
+}
+
+// Overloaded stream insertion operator for Event
+std::ostream& operator<<(std::ostream& os, const Event& event) {
+    os << "Name: " << event.getName() << std::endl;
+    os << "Time: " << event.timeToString() << std::endl;
+    os << "Guests:" << std::endl;
+    for (size_t i = 0; i < event.guests_.size(); ++i) {
+        os << i << ": " << event.guests_[i] << std::endl;
+    }
+    return os;
+}
+
+// Overloaded stream extraction operator for Event
+std::istream& operator>>(std::istream& is, Event& event) {
+    std::string guest;
+    std::getline(is, guest);
+    if (!guest.empty()) {
+        event.guests_.push_back(guest);
+    }
+    return is;
+}
+
+// Add a single guest to the guests list
+bool Event::addGuest(const std::string& name) {
+    if (name.empty()) {
         return false;
     }
     guests_.push_back(name);
     return true;
-
 }
+
+// Add multiple guests to the guests list
 bool Event::addGuest(const std::vector<std::string>& names) {
-    for (size_t i = 0; i < names.size(); ++i) {
-        if (names[i]== " ") {
+    for (const std::string& name : names) {
+        if (name.empty()) {
             return false;
         }
     }
@@ -82,40 +88,35 @@ bool Event::addGuest(const std::vector<std::string>& names) {
     return true;
 }
 
-bool Event::removeGuest(const std::string& name){
-        for (auto desire_Remove = guests_.begin(); desire_Remove != guests_.end(); desire_Remove++) {
-        if (*desire_Remove == name) {
-            guests_.erase(desire_Remove);
-            return true;
-        }
-    }
-    return false;
-}
-        /**
-         * @brief Remove a guest from the event by index
-         * 
-         * @param id Index of the guest to remove
-         * @return true If the index is within the size of the guests list and is removed
-         * @return false If the index is not within the guests list and can't be removed
-         * @post Guest removed from the event if index is within the size of the list and the order of guests is maintained
-         */
-bool Event::removeGuest(const size_t& id){
-    while(id < guests_.size()){
-            guests_.erase(guests_.begin() + id);
-            return true;
+// Remove a guest from the event by name
+bool Event::removeGuest(const std::string& name) {
+    auto it = std::find(guests_.begin(), guests_.end(), name);
+    if (it != guests_.end()) {
+        guests_.erase(it);
+        return true;
     }
     return false;
 }
 
-//TODO: REMOVE THIS COMMENT. DON'T MODIFY THIS FUNCTION IMPLEMENTATION. USE IT IN THE OUTPUT STREAM OPERATOR OVERLOAD.
+// Remove a guest from the event by index
+bool Event::removeGuest(const size_t& id) {
+    if (id < guests_.size()) {
+        guests_.erase(guests_.begin() + id);
+        return true;
+    }
+    return false;
+}
+
+// Convert the event unix timestamp to string format
 std::string Event::timeToString() const {
     // if timestamp is -1, it is not valid and is the result of default constructor
     if (timestamp_ == -1) {
         return "Not Set Yet";
     }
-    struct tm* dt = localtime(&timestamp_);
+    time_t raw_time = static_cast<time_t>(timestamp_);
+    struct tm* dt = localtime(&raw_time);
     std::string time = asctime(dt);
-    time.pop_back();
+    time.pop_back();  // remove trailing newline
     return time;
 }
 
